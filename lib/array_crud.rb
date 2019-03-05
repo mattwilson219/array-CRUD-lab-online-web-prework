c33978bc6cece8a8ed6e7656e3ebd640702c6659
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 18:36:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  create_an_empty_array[]
 end
 
 def create_an_array
-  
+  create_an_array[1, 2 , 3 , "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push
 end
 
 def add_element_to_start_of_array(array, element)
